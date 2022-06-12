1d6af29a6c9e624e1d8420797d2c36a343d438de
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2022-06-12 21:51:00 +0530

    finish style header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,14 +4,14 @@ import HeaderIcon from './HeaderIcon'
 import { HomeIcon,UserIcon,PhoneIcon,InformationCircleIcon } from '@heroicons/react/solid'
 function Header() {
   return (
-    <div>
-<div className="">
+    <div className='bg-gray-700 text-gray-200 flex flex-col items-center  p-6 select-none sm:flex-row justify-between'>
+<div className="flex mb-2 ">
     <HeaderIcon Icon={HomeIcon} title="HOME"/>
     <HeaderIcon Icon={UserIcon} title="ACCOUNT"/>
     <HeaderIcon Icon={PhoneIcon} title="CONTACT"/>
     <HeaderIcon Icon={InformationCircleIcon} title="ABOUT"/>
 </div>
-<Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/575px-IMDB_Logo_2016.svg.png' width={100} height={100}/>
+<Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/575px-IMDB_Logo_2016.svg.png' width={150} height={90}className='cursor-pointer active:brightness-110 '/>
     </div>
   )
 }
