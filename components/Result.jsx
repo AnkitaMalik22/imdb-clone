8c1440781ad6e3734254bde46cde7329cbdea10a
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2022-06-14 09:46:55 +0530

    created server side function and pass it to the client side

--- components/Result.jsx
+++ components/Result.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function Result() {
+function Result({results}) {
+  console.log(results);
   return (
-    <div>Result</div>
+    <div>{results.map((result)=>(
+    <h1>{result.title}</h1>
+    ))}</div>
   )
 }
-
-export default Result
+export default Result;
